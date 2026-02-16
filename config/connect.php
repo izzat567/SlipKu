5e7f40b4f1874e32139cc151666d6e6121df5337
syazrin hadri <syazrinhadri125@gmail.com>
2026-02-16 23:38:44 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,30 +1,7 @@
 <?php
 // config/connect.php
 
-<<<<<<< HEAD
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'slipku_db'; // Tukar nama variable
 
-// Create connection
-$database = new mysqli($host, $user, $password, $dbname); // $database sekarang adalah object mysqli
-
-// Check connection
-if ($database->connect_error) {
-    die("Connection failed: " . $database->connect_error);
-}
-
-// Set charset
-$database->set_charset("utf8mb4");
-
-// Optional: Set timezone
-date_default_timezone_set('Asia/Kuala_Lumpur');
-
-// Return connection object
-return $database;
-=======
->>>>>>> 43ff998a8910218b439c6f6ffc9faea894d37abd
 // Show all errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -35,7 +12,7 @@ $user = 'root';
 $password = 'danialdev';
 $dbname = 'slipku_db';
 
-// Create connection - GUNA $conn bukan $conn
+// Create connection
 $conn = new mysqli($host, $user, $password, $dbname);
 
 // Check connection
@@ -48,4 +25,6 @@ $conn->set_charset("utf8mb4");
 
 // Set timezone
 date_default_timezone_set('Asia/Kuala_Lumpur');
+
+// Jangan return, biarkan variable $conn tersedia
 ?>
